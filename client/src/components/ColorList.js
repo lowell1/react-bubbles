fe95d06b8f157bd042272f9d8226c14e68ca5cc3
lowell1 <lowelljacobs@mail.com>
2019-10-18 10:05:26 -0700

    login form + delete colors

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
-import axios from "axios";
+import {axiosWithAuth} from "../axiosWithAuth";
 
 const initialColor = {
   color: "",
   code: { hex: "" }
 };
 
-const ColorList = ({ colors, updateColors }) => {
-  console.log(colors);
+const ColorList = (props) => {
+  console.log(props.colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
 
@@ -21,17 +21,23 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+
+    console.log(colorToEdit)
+
+    // axiosWithAuth().put()
   };
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    axiosWithAuth().delete(`/api/colors/${color.id}`)
+    .catch(err => console.log(err.response));
   };
 
   return (
     <div className="colors-wrap">
       <p>colors</p>
       <ul>
-        {colors.map(color => (
+        {props.colors.map(color => (
           <li key={color.color} onClick={() => editColor(color)}>
             <span>
               <span className="delete" onClick={() => deleteColor(color)}>
